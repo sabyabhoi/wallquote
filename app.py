4476923df0aa02061453aef6f86d753944f0354e
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2023-06-04 15:23:20 +0530

    Add quotes list

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import glob
 import random
@@ -15,43 +14,33 @@ class ImageModifier:
 
     def read_config_from_json(self, filepath):
         with open(filepath) as f:
-            data = json.load(f)
-            self.wallpaper_dir = data['wallpaper_dir']
-            self.output_file = data['output_file']
-            self.font = ImageFont.truetype(data['font'], 40)
+            config = json.load(f)
+            self.wallpaper_dir = config['wallpaper_dir']
+            self.output_file = config['output_file']
+            self.font = config['font']
+            self.fontsize = config['fontsize']
+            self.msg_file = config['msg_file']
 
     def exec_command(self):
-        for f in [self.input_file, self.msg, self.output_file]:
-            if len(f) == 0:
-                raise ValueError("Either of inputfile, msg or outputfile params is \
-                empty")
-                return
-        if not os.path.exists(self.input_file):
-            raise FileNotFoundError(f"{self.input_file} not found.")
+        for f in [self.input_file, self.msg_file]:
+            if not os.path.exists(f):
+                raise FileNotFoundError(f"{f} not found")
 
-        self.msg = '\n'.join(textwrap.wrap(self.msg, width=40))
+        chosen = random.choice(list(open(self.msg_file)))
+        self.msg = '\n'.join(textwrap.wrap(chosen, width=40))
 
         WIDTH, HEIGHT = 1920, 1080
         img = Image.open(self.input_file).resize((WIDTH, HEIGHT))
         draw = ImageDraw.Draw(img)
         x, y = WIDTH//2, HEIGHT//2
-        draw.text((x - 5, y + 5), self.msg, (0, 0, 0), font=self.font, anchor='mm')
-        draw.text((x, y), self.msg, fill=(255, 255, 255), font=self.font, anchor='mm')
+        fontObj = ImageFont.truetype(self.font, self.fontsize)
+        draw.text((x + 5, y + 5), self.msg, (0, 0, 0), font=fontObj, anchor='mm')
+        draw.text((x, y), self.msg, fill=(255, 255, 255), font=fontObj, anchor='mm')
         img.save(self.output_file)
 
     def get_random(self):
         imgs = glob.glob(self.wallpaper_dir + '*.*')
         self.input_file = random.choice(imgs)
-
-    def get_args(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('inputfile')
-        parser.add_argument('msg')
-        parser.add_argument('outputfile')
-        args = parser.parse_args()
-        self.input_file = args.inputfile
-        self.msg = args.msg
-        self.output_file = args.outputfile
 
     def set_bg(self):
         self.get_random()
