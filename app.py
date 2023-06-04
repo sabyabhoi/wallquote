1d682365ee64e69bcc56675e4c7afcd8b8990ea7
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2023-06-04 14:56:55 +0530

    Use pillow module instead of imagemagick

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import argparse
 import os
+import glob
+import random
+import textwrap
+from PIL import Image, ImageDraw, ImageFont
 
 class ImageModifier:
     def __init__(self):
-        self.inputfile = "image.jpg"
-        self.msg = "Hello sailor"
+        self.inputfile = ""
+        self.msg = "Do the hard work, even when you don\'t feel like it"
         self.outputfile = "output.jpg"
 
     def exec_command(self):
-        font = '/home/cognusboi/.local/share/fonts/Spock/spockess_black.otf'
-
         for f in [self.inputfile, self.msg, self.outputfile]:
             if len(f) == 0:
                 raise ValueError("Either of inputfile, msg or outputfile params is \
@@ -18,11 +20,20 @@ class ImageModifier:
         if not os.path.exists(self.inputfile):
             raise FileNotFoundError(f"{self.inputfile} not found.")
 
-        cmd = f'convert {self.inputfile} -gravity center -pointsize 200\
-        -font {font}\
-        -fill white -annotate +0+0 "{self.msg}" {self.outputfile}'
-        os.system(cmd)
-        
+        self.msg = '\n'.join(textwrap.wrap(self.msg, width=40))
+
+        font = ImageFont.truetype('/usr/share/fonts/TTF/RobotoMono-Bold.ttf', 40)
+
+        img = Image.open(self.inputfile).resize((1920, 1080))
+        draw = ImageDraw.Draw(img)
+        x, y = 1920//2, 1080//2
+        draw.text((x - 5, y + 5), self.msg, (0, 0, 0), font=font, anchor='mm')
+        draw.text((x, y), self.msg, fill=(255, 255, 255), font=font, anchor='mm')
+        img.show()
+
+    def get_random(self, dir):
+        imgs = glob.glob(dir + '*.jpg')
+        self.inputfile = random.choice(imgs)
 
     def get_args(self):
         parser = argparse.ArgumentParser()
@@ -34,8 +45,12 @@ class ImageModifier:
         self.msg = args.msg
         self.outputfile = args.outputfile
 
+    def set_bg(self):
+        cmd = f'feh --bg-scale {self.outputfile}'
+        os.system(cmd)
     
-
 if __name__ == '__main__':
     image_modifier = ImageModifier()
+    image_modifier.get_random('/home/cognusboi/workspace/userfiles/Media/Pictures/wallpapers/')
     image_modifier.exec_command()
+#    image_modifier.set_bg()
