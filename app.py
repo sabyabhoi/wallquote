d22d46592869e93f60f2e46ec39441ead5bd1ebb
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2023-06-04 12:38:49 +0530

    Add gitignore and core imagemagick command

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 import argparse
+import os
 
 class ImageModifier:
     def __init__(self):
-        self.inputfile = ""
+        self.inputfile = "image.jpg"
         self.msg = "Hello sailor"
         self.outputfile = "output.jpg"
 
     def exec_command(self):
         font = '/home/cognusboi/.local/share/fonts/Spock/spockess_black.otf'
 
+        for f in [self.inputfile, self.msg, self.outputfile]:
+            if len(f) == 0:
+                raise ValueError("Either of inputfile, msg or outputfile params is \
+                empty")
+                return
+        if not os.path.exists(self.inputfile):
+            raise FileNotFoundError(f"{self.inputfile} not found.")
+
         cmd = f'convert {self.inputfile} -gravity center -pointsize 200\
         -font {font}\
         -fill white -annotate +0+0 "{self.msg}" {self.outputfile}'
-        print(cmd)
+        os.system(cmd)
         
 
     def get_args(self):
