92986e73cd8b7dbf81cfb8f5448b82dbbd6e750e
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2023-06-04 15:07:13 +0530

    Add JSON configuration support

--- app.py
+++ app.py
@@ -3,37 +3,45 @@ import os
 import glob
 import random
 import textwrap
+import json
 from PIL import Image, ImageDraw, ImageFont
 
 class ImageModifier:
     def __init__(self):
-        self.inputfile = ""
+        self.input_file = ""
         self.msg = "Do the hard work, even when you don\'t feel like it"
-        self.outputfile = "output.jpg"
+        self.output_file = "output.jpg"
+        self.wallpaper_dir = ""
+
+    def read_config_from_json(self, filepath):
+        with open(filepath) as f:
+            data = json.load(f)
+            self.wallpaper_dir = data['wallpaper_dir']
+            self.output_file = data['output_file']
+            self.font = ImageFont.truetype(data['font'], 40)
 
     def exec_command(self):
-        for f in [self.inputfile, self.msg, self.outputfile]:
+        for f in [self.input_file, self.msg, self.output_file]:
             if len(f) == 0:
                 raise ValueError("Either of inputfile, msg or outputfile params is \
                 empty")
                 return
-        if not os.path.exists(self.inputfile):
-            raise FileNotFoundError(f"{self.inputfile} not found.")
+        if not os.path.exists(self.input_file):
+            raise FileNotFoundError(f"{self.input_file} not found.")
 
         self.msg = '\n'.join(textwrap.wrap(self.msg, width=40))
 
-        font = ImageFont.truetype('/usr/share/fonts/TTF/RobotoMono-Bold.ttf', 40)
-
-        img = Image.open(self.inputfile).resize((1920, 1080))
+        WIDTH, HEIGHT = 1920, 1080
+        img = Image.open(self.input_file).resize((WIDTH, HEIGHT))
         draw = ImageDraw.Draw(img)
-        x, y = 1920//2, 1080//2
-        draw.text((x - 5, y + 5), self.msg, (0, 0, 0), font=font, anchor='mm')
-        draw.text((x, y), self.msg, fill=(255, 255, 255), font=font, anchor='mm')
-        img.show()
+        x, y = WIDTH//2, HEIGHT//2
+        draw.text((x - 5, y + 5), self.msg, (0, 0, 0), font=self.font, anchor='mm')
+        draw.text((x, y), self.msg, fill=(255, 255, 255), font=self.font, anchor='mm')
+        img.save(self.output_file)
 
-    def get_random(self, dir):
-        imgs = glob.glob(dir + '*.jpg')
-        self.inputfile = random.choice(imgs)
+    def get_random(self):
+        imgs = glob.glob(self.wallpaper_dir + '*.*')
+        self.input_file = random.choice(imgs)
 
     def get_args(self):
         parser = argparse.ArgumentParser()
@@ -41,16 +49,17 @@ class ImageModifier:
         parser.add_argument('msg')
         parser.add_argument('outputfile')
         args = parser.parse_args()
-        self.inputfile = args.inputfile
+        self.input_file = args.inputfile
         self.msg = args.msg
-        self.outputfile = args.outputfile
+        self.output_file = args.outputfile
 
     def set_bg(self):
-        cmd = f'feh --bg-scale {self.outputfile}'
+        self.get_random()
+        self.exec_command()
+        cmd = f'feh --bg-scale {self.output_file}'
         os.system(cmd)
     
 if __name__ == '__main__':
     image_modifier = ImageModifier()
-    image_modifier.get_random('/home/cognusboi/workspace/userfiles/Media/Pictures/wallpapers/')
-    image_modifier.exec_command()
-#    image_modifier.set_bg()
+    image_modifier.read_config_from_json('config.json')
+    image_modifier.set_bg()
